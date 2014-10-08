b4771f089d28e2d0e7c4a2ea72e3a885d284f458
Hideyuki Kodama <hidek84+tech@gmail.com>
2014-10-08 20:47:31 +0900

    所要時間のfieldと表示部分を追加

--- app/models/card/recipe_card.rb
+++ app/models/card/recipe_card.rb
@@ -1,4 +1,6 @@
 class Card::RecipeCard < Card
+  field :required_time, type: Fixnum, default: 0
+
   include Mongoid::Orderable
   embedded_in :recipe
   orderable column: :position
@@ -7,7 +9,7 @@ class Card::RecipeCard < Card
 
   class << self
     def updatable_columns
-      super + [:position, :move_to]
+      super + [:position, :move_to, :required_time]
     end
   end
 end
